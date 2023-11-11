f115eaf31e69884d1d0b6a425f30ee1c0415df07
firacil <firaolmekbib511@gmail.com>
2023-11-11 07:53:02 -0800

    printf updated

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL || format[i] == NULL)
+	if (!format[i] || (format[i + 1] == ' ' && !format[2]))
 		return (-1);
 
 	for (i = 0; format && format[i] != '\0'; i++)
