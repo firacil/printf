74cb9d2a81526536efc6e67d92ace35bb71c6d0a
firacil <firaolmekbib511@gmail.com>
2023-11-11 04:21:49 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -21,7 +21,6 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -46,7 +45,6 @@ int _printf(const char *format, ...)
 		}
 		count_c += 1;
 	}
-
 	va_end(args);
 	return (count_c);
 }
