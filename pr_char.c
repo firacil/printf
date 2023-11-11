10cc85507eccbd3822cc6bf7485fad553e1d9c8b
firacil <firaolmekbib511@gmail.com>
2023-11-11 11:13:32 -0800

    pud

--- pr_char.c
+++ pr_char.c
@@ -7,10 +7,10 @@
  * Return: printed charchter.
  */
 
-int my_chr(va_list args, int count)
+int my_chr(va_list args)
 {
 	int c = va_arg(args, int);
 
 	my_putc(c);
-	return (count + 1);
+	return (1);
 }
