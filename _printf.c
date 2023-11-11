438e6eb068343f5af354d8ff8b24ba210336b2ff
firacil <firaolmekbib511@gmail.com>
2023-11-11 07:33:20 -0800

    printf updated

--- _printf.c
+++ _printf.c
@@ -14,9 +14,9 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	//if (!format || (format[0] == '%' && !format[1]))
+	if (!format || (format[0] == '%' && !format[1]))
 		//return (-1);
-	//if (format[0] == '%' && (format[1] == ' ' && !format[2]))
+	if (format[0] == '%' && (format[1] == ' ' && !format[2]))
 		//return (-1);
 
 	for (i = 0; format && format[i] != '\0'; i++)
@@ -26,23 +26,26 @@ int _printf(const char *format, ...)
 			my_putc(format[i]);
 		}
 
-		else if (format[i + 1] == 'c')
+		while (format[i] == '%')
 		{
-			my_putc(va_arg(args, int));
-			i++;
+			if (format[i + 1] == 'c')
+			{
+				my_putc(va_arg(args, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				s_count = my_puts(va_arg(args, char *));
+				i++;
+				count_c += (s_count - 1);
+			}
+			else if (format[i] == '%' && format[i + 1] == '%')
+			{
+				my_putc('%');
+				i++;
+			}
+			count_c += 1;
 		}
-		else if (format[i + 1] == 's')
-		{
-			s_count = my_puts(va_arg(args, char *));
-			i++;
-			count_c += (s_count - 1);
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			my_putc('%');
-			i++;
-		}
-		count_c += 1;
 	}
 	va_end(args);
 	return (count_c);
