81b103695ec8889fbec55072c094401eea478f87
firacil <firaolmekbib511@gmail.com>
2023-11-11 22:52:38 -0800

    update

--- main.h
+++ main.h
@@ -15,5 +15,5 @@ int my_chr(va_list args, int count);
 int selectr(const char *format, va_list args, int pr);
 int _printf(const char *format, ...);
 int integer(va_list args, int pr);
-
+int pr_bin(unsigned int num, int pr);
 #endif
