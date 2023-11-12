363024a5c1b26485a3bcf7f3395aa1407967b306
firacil <firaolmekbib511@gmail.com>
2023-11-12 13:28:33 -0800

    test

--- pr_char.c
+++ pr_char.c
@@ -3,15 +3,13 @@
 /**
  * my_chr - prints char.
  * @args: number of arguments
- * @i: is 1.
+ * @i: is integer.
  * Return: print success
  */
 
 int my_chr(va_list args, int i)
 {
 	char c = va_arg(args, int);
-
-	i = 1;
 
 	my_putc(c);
 	return (i += 1);
