08cd155d8cca84ded083c8fe2e94d5dddede2bb4
firacil <firaolmekbib511@gmail.com>
2023-11-12 10:18:44 -0800

    test

--- printf.c
+++ printf.c
@@ -15,6 +15,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+	if (format[i] == NULL)
+		return (-1);
 
 	while (*format != '\0')
 	{
